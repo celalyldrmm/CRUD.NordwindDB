5fd730136b073914121f4853adf6f07904af5ff5
Celal Yıldırım <celallee12@gmail.com>
2024-04-03 17:41:01 +0300

    Add project files.

---
+++ UI/FrmMenu.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CRUDNordwindDB.UI.Forms.Category;
+using CRUDNordwindDB.UI.Forms.Supplier;
+using CRUDNordwindDB.UI.Forms.Product;
+
+namespace CRUDNordwindDB.UI
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kategoriGirişToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmCategoryInsert frmCategoryInsert = new FrmCategoryInsert();
+            frmCategoryInsert.ShowDialog();
+        }
+
+        private void markaGirişToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmSupplierInsert frmSupplierInsert = new FrmSupplierInsert();
+            frmSupplierInsert.ShowDialog();
+        }
+
+        private void ürünGirişToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmProductInsert frmProductInsert = new FrmProductInsert();
+            frmProductInsert.ShowDialog();
+        }
+
+        private void kATEGORİLİSTELEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmCategorySelect frmCategorySelect = new FrmCategorySelect();
+            frmCategorySelect.ShowDialog();
+        }
+
+        private void lİSTELEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmProductSelect frmProductSelect = new frmProductSelect();
+            frmProductSelect.ShowDialog();
+        }
+
+        private void mARKALİSTELEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmSupplierSelect frmSupplierSelect = new FrmSupplierSelect();
+            frmSupplierSelect.ShowDialog();
+        }
+    }
+}
