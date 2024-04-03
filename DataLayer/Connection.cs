b8a8f151d04fa0d894991cb1dc6a6c68a935dabc
Celal Yıldırım <celallee12@gmail.com>
2024-04-03 17:43:24 +0300

    Feature: Added SQL Connection

---
+++ DataLayer/Connection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Connection
+    {
+        public static SqlConnection baglanti
+        {
+            get
+            {
+                SqlConnection sqlcon = new SqlConnection
+                    ("Server=DESKTOP-VUJBGC1\\SQLEXPRESS;Trusted_Connection=True;Database=NORTHWND;");
+                return sqlcon;
+            }
+        }
+
+    }
+}
